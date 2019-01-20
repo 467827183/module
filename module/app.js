2a83d9f838cbf5911a0607e987e8199003d27c90
wpl <467827183@qq.com>
2019-01-20 19:27:54 +0800

    '第二次提交'

--- module/app.js
+++ module/app.js
@@ -1,9 +1,18 @@
 let module1=require('./module1');
 let module2=require('./module2');
 let module3=require('./module3');
-
+let fs=require('fs');
 module1.cn();
 module1.fn();
 module2();
 module3.mode();
 module3.ccd();
+fs.writeFile('./hello.txt','66666',{
+  flag:'w',
+  mode:'0o666',
+  encoding:'utf-8'
+},(err)=>{
+  if(!err) console.log('成功')
+  else console.log('失败')
+})
+
